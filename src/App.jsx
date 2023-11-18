791995150818d9fd2c1ed37652ca2efd5326d400
MatiasSimonLuduena <matipax04@gmail.com>
2023-11-18 03:07:20 -0300

    final

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <img src="image-qr-code.png" alt="image qr" />
+          <h1>Improve your front-end skills by building projects</h1>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+          </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
